8cdd4450b0f22c3a657b4e7a58c6705b35943ac6
liadiluz8 <liadiluz@mail.tau.ac.il>
2023-06-04 18:29:57 +0300

    liad-eclipse-run

--- src/app/SudokuController.java
+++ src/app/SudokuController.java
@@ -77,7 +77,7 @@ public class SudokuController {
 		Integer[][] medBoard = {{null, null, null, null, 6, null, 7, null, 1},
 														 {6, null, null, null, 7, null, null, 9, null},
 														 {1, 9, null, null, null, null, 5, null, null},
-														 {8, 2, null, 1, null, null, null, 4, 0},
+														 {8, 2, null, 1, null, null, null, 4, 7},
 														 {null, null, 4, 6, null, 2, 9, null, null},
 														 {null, 5, null, null, null, 3, null, 2, null},
 														 {null, null, 9, 3, null, null, null, 7, 4},
